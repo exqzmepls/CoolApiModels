926ffc1a4809b556417ac5bc469e5313eb315da1
exqzmepls <exqzmepls@yandex.ru>
2022-06-14 19:33:49 +0500

    Add AuthenticatedResult

---
+++ CoolApiModels/Users/AuthenticatedResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoolApiModels.Users
+{
+    public class AuthenticatedResult
+    {
+        public Guid UserId { get; set; }
+
+        public string Token { get; set; }
+    }
+}
